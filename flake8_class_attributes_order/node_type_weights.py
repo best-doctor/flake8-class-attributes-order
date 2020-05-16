ccff258da6b256bc0be018accab8b00770a76414
dumbturtle <semenlinux@gmail.com>
2020-05-16 13:31:57 +0300

    Refactoring node_type_weights.py
    Modified:   flake8_class_attributes_order/node_type_weights.py : Remove class ClassNodeTypeWeights. Add global vars.

--- flake8_class_attributes_order/node_type_weights.py
+++ flake8_class_attributes_order/node_type_weights.py
@@ -4,105 +4,104 @@ from typing import List, Mapping, Dict
 from typing_extensions import Final
 
 
-class ClassNodeTypeWeights:
-    NON_STRICT_NODE_TYPE_WEIGHTS: Final[Mapping[str, int]] = {
-        'docstring': 0,
-        'pass': 1,
-        'meta_class': 2,
-        'nested_class': 3,
+non_strict_node_type_weights: Final[Mapping[str, int]] = {
+    'docstring': 0,
+    'pass': 1,
+    'meta_class': 2,
+    'nested_class': 3,
 
-        'constant': 4,
-        'field': 5,
-        'outer_field': 6,
+    'constant': 4,
+    'field': 5,
+    'outer_field': 6,
 
-        'if': 7,
-        'expression': 8,
+    'if': 7,
+    'expression': 8,
 
-        '__new__': 9,
-        '__init__': 10,
-        '__post_init__': 11,
-        '__str__': 12,
+    '__new__': 9,
+    '__init__': 10,
+    '__post_init__': 11,
+    '__str__': 12,
 
-        'save': 13,
-        'delete': 14,
+    'save': 13,
+    'delete': 14,
 
-        'property_method': 20,
-        'private_property_method': 20,
-        'static_method': 22,
-        'private_static_method': 22,
-        'class_method': 24,
-        'private_class_method': 24,
-        'method': 26,
-        'magic_method': 27,
-        'private_method': 27,
-    }
+    'property_method': 20,
+    'private_property_method': 20,
+    'static_method': 22,
+    'private_static_method': 22,
+    'class_method': 24,
+    'private_class_method': 24,
+    'method': 26,
+    'magic_method': 27,
+    'private_method': 27,
+}
 
-    STRICT_NODE_TYPE_WEIGHTS: Final[Mapping[str, int]] = {
-        'docstring': 0,
-        'pass': 1,
-        'meta_class': 2,
-        'nested_class': 3,
+strict_node_type_weights: Final[Mapping[str, int]] = {
+    'docstring': 0,
+    'pass': 1,
+    'meta_class': 2,
+    'nested_class': 3,
 
-        'constant': 4,
-        'field': 5,
-        'outer_field': 6,
+    'constant': 4,
+    'field': 5,
+    'outer_field': 6,
 
-        'if': 7,
-        'expression': 8,
+    'if': 7,
+    'expression': 8,
 
-        '__new__': 9,
-        '__init__': 10,
-        '__post_init__': 11,
-        '__str__': 12,
+    '__new__': 9,
+    '__init__': 10,
+    '__post_init__': 11,
+    '__str__': 12,
 
-        'save': 13,
-        'delete': 14,
+    'save': 13,
+    'delete': 14,
 
-        'property_method': 20,
-        'private_property_method': 21,
-        'static_method': 22,
-        'private_static_method': 23,
-        'class_method': 24,
-        'private_class_method': 25,
-        'method': 26,
-        'magic_method': 27,
-        'private_method': 28,
-    }
+    'property_method': 20,
+    'private_property_method': 21,
+    'static_method': 22,
+    'private_static_method': 23,
+    'class_method': 24,
+    'private_class_method': 25,
+    'method': 26,
+    'magic_method': 27,
+    'private_method': 28,
+}
 
-    FIXED_NODE_TYPE_WEIGHTS: Final[Dict[str, int]] = {
-        'docstring': 0,
-        'pass': 1,
-        'expression': 2,
-        'if': 3,
-    }
+fixed_node_type_weights: Final[Dict[str, int]] = {
+    'docstring': 0,
+    'pass': 1,
+    'expression': 2,
+    'if': 3,
+}
 
-    CONFIGURABLE_NODE_TYPES: Final[Mapping[str, List[str]]] = {
-        'nested_class': ['nested_class'],
-        'meta_class': ['meta_class', 'nested_class'],
+configurable_node_types: Final[Mapping[str, List[str]]] = {
+    'nested_class': ['nested_class'],
+    'meta_class': ['meta_class', 'nested_class'],
 
-        'field': ['field'],
-        'constant': ['constant', 'field'],
-        'outer_field': ['outer_field', 'field'],
+    'field': ['field'],
+    'constant': ['constant', 'field'],
+    'outer_field': ['outer_field', 'field'],
 
-        'method': ['method'],
-        'magic_method': ['magic_method', 'method'],
-        '__new__': ['__new__', 'magic_method', 'method'],
-        '__init__': ['__init__', 'magic_method', 'method'],
-        '__post_init__': ['__post_init__', 'magic_method', 'method'],
-        '__str__': ['__str__', 'magic_method', 'method'],
+    'method': ['method'],
+    'magic_method': ['magic_method', 'method'],
+    '__new__': ['__new__', 'magic_method', 'method'],
+    '__init__': ['__init__', 'magic_method', 'method'],
+    '__post_init__': ['__post_init__', 'magic_method', 'method'],
+    '__str__': ['__str__', 'magic_method', 'method'],
 
-        'private_method': ['private_method', 'method'],
+    'private_method': ['private_method', 'method'],
 
-        'save': ['save', 'method'],
-        'delete': ['delete', 'method'],
+    'save': ['save', 'method'],
+    'delete': ['delete', 'method'],
 
-        'property_method': ['property_method', 'method'],
-        'private_property_method': ['private_property_method', 'property_method', 'method'],
-        'static_method': ['static_method', 'method'],
-        'private_static_method': ['private_static_method', 'static_method', 'method'],
-        'class_method': ['class_method', 'method'],
-        'private_class_method': ['private_class_method', 'class_method', 'method'],
-    }
+    'property_method': ['property_method', 'method'],
+    'private_property_method': ['private_property_method', 'property_method', 'method'],
+    'static_method': ['static_method', 'method'],
+    'private_static_method': ['private_static_method', 'static_method', 'method'],
+    'class_method': ['class_method', 'method'],
+    'private_class_method': ['private_class_method', 'class_method', 'method'],
+}
 
 
 def get_node_weights(options=None) -> Mapping[str, int]:
@@ -118,14 +117,14 @@ def get_node_weights(options=None) -> Mapping[str, int]:
         )
 
     if class_attributes_order:
-        node_type_weights = ClassNodeTypeWeights.FIXED_NODE_TYPE_WEIGHTS.copy()
+        node_type_weights = fixed_node_type_weights.copy()
         node_to_configured_weight = {
             node_type: weight for weight, node_type in enumerate(
                 class_attributes_order,
                 start=len(node_type_weights))
         }
 
-        for node_type, node_type_path in ClassNodeTypeWeights.CONFIGURABLE_NODE_TYPES.items():
+        for node_type, node_type_path in configurable_node_types.items():
             for node_type_or_supertype in node_type_path:
                 if node_type_or_supertype in node_to_configured_weight:
                     node_type_weights[node_type] = node_to_configured_weight[node_type_or_supertype]
@@ -135,6 +134,6 @@ def get_node_weights(options=None) -> Mapping[str, int]:
 
     if use_strict_mode:
 
-        return ClassNodeTypeWeights.STRICT_NODE_TYPE_WEIGHTS
+        return strict_node_type_weights
 
-    return ClassNodeTypeWeights.NON_STRICT_NODE_TYPE_WEIGHTS
+    return non_strict_node_type_weights
